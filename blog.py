5f0ee8daf366bbfb1f36bc761eb6d94e7ec9d08a
Giovanni <giovannibarbarani@gmail.com>
2015-10-02 11:55:58 +0200

    Decorator line 54 seems not working

--- blog.py
+++ blog.py
@@ -3,6 +3,7 @@
 #imports
 
 from flask import *
+from functools import wraps
 import sqlite3
 
 #configuration
@@ -23,6 +24,19 @@ def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
 
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if logged_in in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+		return wrap
+
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
 	error=None
@@ -37,9 +51,15 @@ def login():
 	return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run()
