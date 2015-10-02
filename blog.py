cc18ced1ca519acbab04fabe53e94dba9de89b1b
Giovanni <giovannibarbarani@gmail.com>
2015-10-02 12:19:15 +0200

    Errors are be resolved: line 35 wrong indentation and line 30 wrong sintax blog.py

--- blog.py
+++ blog.py
@@ -27,12 +27,12 @@ def connect_db():
 def login_required(test):
 	@wraps(test)
 	def wrap(*args, **kwargs):
-		if logged_in in session:
+		if session['logged_in']:
 			return test(*args, **kwargs)
 		else:
 			flash('You need to login first.')
 			return redirect(url_for('login'))
-		return wrap
+	return wrap
 
 
 
